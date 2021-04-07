64004427299e602e5085dca9d8c267a8f31bd61e
dandybytes <42968783+dandybytes@users.noreply.github.com>
2021-04-07 09:24:15 +0200

    Correct NavGroup styling; make SideBar fixed; make page content vertically scrollable

--- packages/client-ui/src/components/common/TopPageLayout.styles.ts
+++ packages/client-ui/src/components/common/TopPageLayout.styles.ts
@@ -2,19 +2,19 @@ import { makeStyles } from '@material-ui/core/styles'
 
 export default makeStyles(theme => ({
   root: {
-    height: '100vh',
+    minHeight: '100vh',
     width: '100vw',
-    display: 'grid',
-    gridTemplateColumns: `fit-content(${theme.layout.sideBarFullWidth}) auto`,
-    gridTemplateAreas: `'sidebar page'`,
-    '&.collapsed': {
-      gridTemplateColumns: `fit-content(${theme.layout.sideBarCollapsedWidth}) auto`
+    '&.collapsed $pageContent': {
+      marginLeft: `${theme.layout.sideBarCollapsedWidth}`
     }
   },
   pageContent: {
-    gridArea: 'page',
-    height: '100vh',
-    minWidth: `calc(100vw - ${theme.layout.sideBarFullWidth}px)`,
-    padding: theme.spacing(3)
+    minHeight: '100vh',
+    marginLeft: `${theme.layout.sideBarFullWidth}`,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.standard
+    })
   }
 }))
--- packages/client-ui/src/components/common/navigation/SideBar.styles.ts
+++ packages/client-ui/src/components/common/navigation/SideBar.styles.ts
@@ -2,9 +2,9 @@ import { makeStyles } from '@material-ui/core/styles'
 
 export default makeStyles(theme => ({
   root: {
+    position: 'fixed',
     height: '100vh',
     width: theme.layout.sideBarFullWidth,
-    gridArea: 'sidebar',
     display: 'flex',
     flexDirection: 'column',
     transition: theme.transitions.create('width', {
