ebffc6d2bbb5062605ac61a7c1e1f8dc6287782d
Roman Kalyakin <roman@kalyakin.com>
2021-04-21 19:01:39 +0200

    using setuptools suggestion

--- backend/jupyter-middleware/setup.py
+++ backend/jupyter-middleware/setup.py
@@ -32,7 +32,7 @@ setup_args = dict(
         'stringcase>=1.2.0',
         'pyyaml',
         'pyarrow==3.0.0',
-        'kiara@http://github.com/DHARPA-Project/kiara/tarball/develop#egg=kiara'
+        'kiara@git+https://github.com/DHARPA-Project/kiara.git#egg=kiara'
     ],
     zip_safe=False,
     include_package_data=True,
