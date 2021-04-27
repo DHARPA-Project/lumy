ea1b4a300aa9d4820db16b9826aaf0986a2a2127
Roman Kalyakin <roman@kalyakin.com>
2021-04-27 14:26:07 +0200

    added some styling to the toy VRE

--- packages/toy-vre/src/components/App.tsx
+++ packages/toy-vre/src/components/App.tsx
@@ -1,25 +1,31 @@
 import React from 'react'
 import { useCurrentWorkflow } from '@dharpa-vre/client-core'
-import { WorkflowPreview } from './WorkflowPreview'
+import { Box, Grid, LinearProgress, Typography } from '@material-ui/core'
 import { StatusView } from './StatusView'
+import { WorkflowPreview } from './WorkflowPreview'
+
+const LoadingView = () => {
+  return (
+    <Grid container style={{ flexGrow: 1 }} direction="column" justify="center" alignItems="center">
+      <Typography variant="h6" component="h2" align="center">
+        Loading workflow
+      </Typography>
+      <LinearProgress style={{ width: '100%' }} />
+    </Grid>
+  )
+}
 
 export const App = (): JSX.Element => {
   const [workflow] = useCurrentWorkflow()
 
-  const statusView = <StatusView />
-
-  if (workflow == null)
-    return (
-      <>
-        <p>loading workflow...</p>
-        {statusView}
-      </>
-    )
-
   return (
     <>
-      <WorkflowPreview workflow={workflow} />
-      {statusView}
+      <div style={{ overflow: 'auto', flexGrow: 1, display: 'flex' }}>
+        {workflow == null ? <LoadingView /> : <WorkflowPreview workflow={workflow} />}
+      </div>
+      <Box pt={1}>
+        <StatusView />
+      </Box>
     </>
   )
 }
