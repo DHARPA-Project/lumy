76b393eaee1e458a15eff585eed40c2c0b160a2d
Roman Kalyakin <roman@kalyakin.com>
2021-04-28 09:12:23 +0200

    moved network graph into monorepo (#27)

--- applications/standalone/babel.config.js
+++ applications/standalone/babel.config.js
@@ -8,6 +8,7 @@ module.exports = {
       }
     ],
     '@babel/plugin-proposal-class-properties',
-    ['@babel/plugin-transform-typescript', { allowNamespaces: true }]
+    ['@babel/plugin-transform-typescript', { allowNamespaces: true }],
+    ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }]
   ]
 }
