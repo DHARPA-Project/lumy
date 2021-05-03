acb48081e52b4e7d1cc8b4014caebc50ad58764f
dandybytes <42968783+dandybytes@users.noreply.github.com>
2021-05-03 19:35:03 +0200

    Add side drawer

---
+++ packages/client-ui/src/components/common/SideDrawer.tsx
@@ -0,0 +1,42 @@
+import React, { useContext } from 'react'
+
+import { makeStyles } from '@material-ui/core/styles'
+
+import Drawer from '@material-ui/core/Drawer'
+
+import { PageLayoutContext } from '../../context/pageLayoutContext'
+
+const useStyles = makeStyles(theme => ({
+  drawerPaper: {
+    width: '50%',
+    maxWidth: '80%',
+    [theme.breakpoints.down('sm')]: {
+      minWidth: 320
+    }
+  }
+}))
+
+type SideDrawerProps = {
+  anchor: 'bottom' | 'left' | 'right' | 'top'
+  children: React.ReactNode
+}
+
+const SideDrawer = ({ anchor, children }: SideDrawerProps): JSX.Element => {
+  const classes = useStyles()
+
+  const { isSideDrawerOpen, setIsSideDrawerOpen } = useContext(PageLayoutContext)
+
+  return (
+    <Drawer
+      variant="temporary"
+      anchor={anchor}
+      open={isSideDrawerOpen}
+      onClose={() => setIsSideDrawerOpen(false)}
+      classes={{ paper: classes.drawerPaper }}
+    >
+      {children}
+    </Drawer>
+  )
+}
+
+export default SideDrawer
