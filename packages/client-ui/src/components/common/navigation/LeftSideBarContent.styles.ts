9ff6b8719ae378b63d578dac90e673562ae0c043
dandybytes <42968783+dandybytes@users.noreply.github.com>
2021-05-03 19:31:42 +0200

    Update left sidebar style

--- packages/client-ui/src/components/common/navigation/LeftSideBarContent.styles.ts
+++ packages/client-ui/src/components/common/navigation/LeftSideBarContent.styles.ts
@@ -2,13 +2,12 @@ import { makeStyles } from '@material-ui/core/styles'
 
 export default makeStyles(theme => ({
   sideBarTop: {
+    height: theme.layout.navBarTop,
     position: 'relative',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-    paddingTop: theme.spacing(2),
-    paddingBottom: theme.spacing(1),
-    marginBottom: theme.spacing(12)
+    padding: theme.spacing(2)
   },
   logo: {
     display: 'flex',
@@ -19,32 +18,34 @@ export default makeStyles(theme => ({
   sideBarToggleButton: {
     position: 'absolute',
     top: theme.spacing(2),
-    right: '-14px',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: '4px',
-    borderRadius: '50%',
-    border: '1px solid ' + theme.palette.divider,
-    backgroundColor: theme.palette.background.default,
-    cursor: 'pointer',
-    zIndex: 10,
-    '&:hover': {
-      color: theme.palette.background.default,
-      backgroundColor: theme.palette.text.primary
+    right: theme.spacing(1),
+    transition: theme.transitions.create('right', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.standard
+    }),
+    '&.aside': {
+      right: theme.spacing(-2)
     }
   },
   sideBarExpandArrow: {
     fontSize: '1rem',
-    transition: 'transform 0.3s ease',
     transform: 'rotate(0deg)',
+    transition: theme.transitions.create('transform', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.standard
+    }),
     '&.inward': {
       transform: 'rotate(180deg)'
     }
   },
+  navList: {
+    flexGrow: 1,
+    overflowY: 'auto'
+  },
   sideBarBottom: {
-    marginTop: 'auto',
-    paddingBottom: theme.spacing(1),
+    height: theme.spacing(8),
+    padding: theme.spacing(2, 1),
+    justifySelf: 'flex-end',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center'
