c7a91c090c5bc729a601c143b5a0a153baabd50c
Roman Kalyakin <roman@kalyakin.com>
2021-05-24 11:24:13 +0200

    code reshuffled to make powershell testable on mac

--- applications/jupyter-app/src/electron/installer.ts
+++ applications/jupyter-app/src/electron/installer.ts
@@ -1,3 +1,4 @@
+import path from 'path'
 import { WebContents } from 'electron'
 import { FinishMessage, StreamMessage } from '@dharpa-vre/splash-screen'
 
@@ -28,4 +29,64 @@ export class InstallerComm {
       text
     })
   }
+}
+
+const getRunScript = (forcePowerShell = false): string => {
+  const getPath = (filename: string) => path.resolve(__dirname, `../../src/server/scripts/run/${filename}`)
+  switch (process.platform) {
+    case 'darwin':
+      return forcePowerShell ? getPath('win.ps1') : getPath('mac.sh')
+    case 'win32':
+      return getPath('win.ps1')
+    default:
+      throw new Error('No start script for *nix yet.')
+  }
+}
+
+const getInstallScript = (forcePowerShell = false): string => {
+  const getPath = (filename: string) =>
+    path.resolve(__dirname, `../../src/server/scripts/install/${filename}`)
+  switch (process.platform) {
+    case 'darwin':
+      return forcePowerShell ? getPath('win.ps1') : getPath('mac.sh')
+    case 'win32':
+      return getPath('win.ps1')
+    default:
+      throw new Error('No start script for *nix yet.')
+  }
+}
+
+const getExecutable = (forcePowerShell = false): string => {
+  switch (process.platform) {
+    case 'darwin':
+      return forcePowerShell ? 'pwsh' : 'bash'
+    case 'win32':
+      return 'powershell.exe'
+    default:
+      throw new Error('No start script for *nix yet.')
+  }
+}
+
+export const getRunAppAndArgs = (
+  method: 'default' | 'dry-run' | 'skip-conda',
+  forcePowerShell = false
+): [string, string[]] => {
+  const runScript = getRunScript(forcePowerShell)
+  let args: string[] = []
+  switch (method) {
+    case 'dry-run':
+      args = ['--dry-run']
+      break
+    case 'skip-conda':
+      args = ['--skip-conda']
+      break
+    default:
+      args = []
+  }
+  return [getExecutable(forcePowerShell), [runScript].concat(args)]
+}
+
+export const getInstallAppAndArgs = (forcePowerShell = false): [string, string[]] => {
+  const runScript = getInstallScript(forcePowerShell)
+  return [getExecutable(forcePowerShell), [runScript]]
 }
