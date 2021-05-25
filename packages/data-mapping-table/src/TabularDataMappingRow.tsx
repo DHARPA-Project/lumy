ea9e2b0da633c97461d7ea9ce7013c0f5c668605
dandybytes <42968783+dandybytes@users.noreply.github.com>
2021-05-25 14:43:17 +0200

    Multiple style/spacing adjustments

--- packages/data-mapping-table/src/TabularDataMappingRow.tsx
+++ packages/data-mapping-table/src/TabularDataMappingRow.tsx
@@ -106,6 +106,7 @@ const TabularDataMappingRow = ({
                       className={classes.formControl}
                       variant="outlined"
                       size="small"
+                      margin="dense"
                       key={requiredFieldName}
                     >
                       <InputLabel>{requiredFieldName}</InputLabel>
@@ -116,11 +117,8 @@ const TabularDataMappingRow = ({
                         onChange={event =>
                           handleFieldSelection(requiredFieldName, event.target.value as string)
                         }
-                        autoWidth={true}
+                        autoWidth
                       >
-                        <MenuItem key={-1} value="">
-                          none
-                        </MenuItem>
                         {columnNames.map((columnName, index) => (
                           <MenuItem key={index} value={columnName}>
                             {columnName}
