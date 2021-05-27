093d8321051820e296736b46fc4e3327260af4aa
Roman Kalyakin <roman@kalyakin.com>
2021-05-27 10:13:44 +0200

    title input styled

--- packages/notes-components/src/components/NoteViewerEditor.tsx
+++ packages/notes-components/src/components/NoteViewerEditor.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Note } from '@dharpa-vre/client-core'
-import { Grid, TextField, IconButton, Button } from '@material-ui/core'
+import { Grid, TextField, IconButton, Button, Typography } from '@material-ui/core'
 import CloseIcon from '@material-ui/icons/Close'
 import DeleteIcon from '@material-ui/icons/Delete'
 import SaveIcon from '@material-ui/icons/Save'
 import { MarkdownEditorViewer } from './MarkdownEditorViewer'
+import useStyles from './NoteViewerEditor.styles'
+import { formatDistance } from 'date-fns'
+
+const asTimeAgo = (date: Date): string => formatDistance(date, new Date(), { addSuffix: true })
 
 export interface Props {
   note?: Note
@@ -13,6 +17,7 @@ export interface Props {
 export const NoteViewerEditor = ({ note }: Props): JSX.Element => {
   const [title, setTitle] = React.useState(note?.title ?? '')
   const [content, setContent] = React.useState(note?.content ?? '')
+  const classes = useStyles()
 
   React.useCallback(() => setTitle(title), [note?.title ?? ''])
 
@@ -21,16 +26,25 @@ export const NoteViewerEditor = ({ note }: Props): JSX.Element => {
       <Grid item>
         <Grid container wrap="nowrap">
           <TextField
-            style={{ flexGrow: 1 }}
-            label="Title"
+            placeholder="Click to edit title"
+            className={classes.titleField}
             variant="standard"
             value={title}
             onChange={e => setTitle(e.target.value)}
           />
-          <IconButton aria-label="close">
+          <IconButton aria-label="close" edge="end">
             <CloseIcon />
           </IconButton>
         </Grid>
+      </Grid>
+      <Grid item>
+        {note?.createdAt != null ? (
+          <Typography variant="caption" color="textSecondary">
+            {asTimeAgo(note.createdAt)}
+          </Typography>
+        ) : (
+          ''
+        )}
       </Grid>
       <Grid item>
         <MarkdownEditorViewer text={content} onChanged={setContent} />
