fd87eace65f18a2b739c8b01c332c6a35a4ed887
dandybytes <42968783+dandybytes@users.noreply.github.com>
2021-06-01 22:18:40 +0200

    Replace sidebar with speed dial; create additional tool pane

--- packages/client-ui/src/components/common/WorkflowContainer.styles.ts
+++ packages/client-ui/src/components/common/WorkflowContainer.styles.ts
@@ -5,23 +5,43 @@ export default makeStyles(theme => ({
     minHeight: '100vh',
     width: '100%',
     display: 'grid',
-    gridTemplateColumns: 'fit-content(5%) auto'
+    gridTemplateColumns: `fit-content(5%) auto`
   },
   stepContainer: {
-    // height: `calc(100vh - 2 * ${theme.layout.pagePadding})`,
     height: '100vh',
+    maxWidth: '100%',
     position: 'relative',
     marginLeft: 0,
     padding: 0,
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.standard
-    }),
-    '&.right-squeeze': {
-      marginRight: `${theme.layout.toolContainerWidth}`
+    display: 'flex',
+    overflow: 'hidden'
+  },
+  toolAreaToggle: {
+    position: 'absolute',
+    '&.MuiFab-root': {
+      bottom: theme.spacing(2),
+      right: theme.spacing(1)
     },
-    '&.right-pinch': {
-      marginRight: `${theme.layout.toolBarWidth}`
+    '&.MuiSpeedDial-directionUp, &.MuiSpeedDial-directionLeft': {
+      bottom: theme.spacing(2),
+      right: theme.spacing(1)
+    },
+    '&.MuiSpeedDial-directionDown, &.MuiSpeedDial-directionRight': {
+      top: theme.spacing(2),
+      left: theme.spacing(1)
     }
+  },
+  mainPane: {
+    flex: 1,
+    overflow: 'hidden',
+    transition: 'all 0.3s ease'
+  },
+  additionalPane: {
+    flex: 1,
+    overflow: 'hidden'
+  },
+  verticalPaneDivider: {
+    border: `1px solid ${theme.palette.divider}`,
+    cursor: 'col-resize'
   }
 }))
--- packages/client-ui/src/components/common/WorkflowContainer.tsx
+++ packages/client-ui/src/components/common/WorkflowContainer.tsx
@@ -1,39 +1,103 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
 
-import { ThemeProvider } from '@material-ui/core/styles'
+import Fab from '@material-ui/core/fab'
+import SpeedDial from '@material-ui/lab/SpeedDial'
+import SpeedDialIcon from '@material-ui/lab/SpeedDialIcon'
+import SpeedDialAction from '@material-ui/lab/SpeedDialAction'
 
-import { ThemeContext } from '../../context/themeContext'
+import CloseIcon from '@material-ui/icons/Close'
+
 import { WorkflowContext } from '../../context/workflowContext'
 import useStyles from './WorkflowContainer.styles'
+import { featureList } from '../../const/features'
 
-import RightSideBarContainer from './toolbar/RightSideBarContainer'
-import RightSideBarContent from './toolbar/RightSideBarContent'
-import SideDrawer from './SideDrawer'
 import FeatureTabs from './FeatureTabs'
 import VerticalStepIndicator from './VerticalStepIndicator'
 import WorkflowStep from './WorkflowStep'
 import LoadingIndicator from './LoadingIndicator'
 
 const WorkflowContainer = (): JSX.Element => {
-  const classes = useStyles()
+  const stepContainerRef = useRef<HTMLDivElement>(null)
+  const mainPaneRef = useRef<HTMLDivElement>(null)
+  const additionalPaneRef = useRef<HTMLDivElement>(null)
+  const yDividerPos = useRef(0)
+  const xDividerPos = useRef(0)
 
-  const { sidebarTheme } = useContext(ThemeContext)
   const {
-    isRightSideBarVisible,
-    isSideDrawerOpen,
+    setFeatureTabIndex,
     projectSteps,
     activeStep,
     direction,
-    setActiveStep
+    setActiveStep,
+    mainPaneWidth,
+    setMainPaneWidth,
+    mainPaneHeight,
+    setMainPaneHeight,
+    isAdditionalPaneVisible,
+    setIsAdditionalPaneVisible
   } = useContext(WorkflowContext) //prettier-ignore
 
-  /**
-   * TODO: add condition to determine when tool bar should be rendered
-   * (e.g. only on workflow project routes or certain workflow steps)
-   */
-  const toolBarRenderCondition = true
+  const classes = useStyles()
 
-  const handleNext = () => {
+  const [isSpeedDialOpen, setIsSpeedDialOpen] = useState(false)
+
+  useEffect(() => {
+    document.addEventListener('mouseup', onMouseUp)
+    document.addEventListener('mousemove', onMouseMove)
+
+    return () => {
+      document.removeEventListener('mouseup', onMouseUp)
+      document.removeEventListener('mousemove', onMouseMove)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (!mainPaneWidth) return
+
+    mainPaneRef.current.style.minWidth = mainPaneWidth + 'px'
+    mainPaneRef.current.style.maxWidth = mainPaneWidth + 'px'
+  }, [mainPaneWidth])
+
+  const getStepContainerWidth = () => stepContainerRef.current?.getBoundingClientRect()?.width
+
+  const onMouseDown = (event: React.MouseEvent): void => {
+    yDividerPos.current = event.clientY
+    xDividerPos.current = event.clientX
+  }
+
+  const onMouseUp = (): void => {
+    yDividerPos.current = null
+    xDividerPos.current = null
+  }
+
+  const onMouseMove = (event: React.MouseEvent): void => {
+    if (!yDividerPos.current && !xDividerPos.current) return
+
+    setMainPaneHeight(mainPaneHeight + event.clientY - yDividerPos.current)
+    setMainPaneWidth(prevWidth => {
+      const newWidth = prevWidth + event.clientX - xDividerPos.current
+      const stepContainerWidth = getStepContainerWidth()
+      return newWidth <= 0.65 * stepContainerWidth && newWidth >= 0.35 * stepContainerWidth
+        ? newWidth
+        : prevWidth
+    })
+
+    yDividerPos.current = event.clientY
+    xDividerPos.current = event.clientX
+  }
+
+  const handleOpenAdditionalPane = () => {
+    setMainPaneWidth(getStepContainerWidth() / 2)
+    setIsAdditionalPaneVisible(true)
+  }
+
+  const handleCloseAdditionalPane = () => {
+    setIsAdditionalPaneVisible(false)
+    setMainPaneWidth(getStepContainerWidth())
+  }
+
+  const handleNextStepClick = () => {
     setActiveStep(([prevActiveStep, prevDirection]) => {
       const nextStep = prevActiveStep + 1
       if (nextStep >= projectSteps.length) return [prevActiveStep, prevDirection]
@@ -41,7 +105,7 @@ const WorkflowContainer = (): JSX.Element => {
     })
   }
 
-  const handleBack = () => {
+  const handlePreviousStepClick = () => {
     setActiveStep(([prevActiveStep, prevDirection]) => {
       const nextStep = prevActiveStep - 1
       if (nextStep < 0) return [prevActiveStep, prevDirection]
@@ -52,37 +116,81 @@ const WorkflowContainer = (): JSX.Element => {
   if (!projectSteps?.length) return <LoadingIndicator />
 
   return (
-    <div className={classes.workflowContainer}>
-      <VerticalStepIndicator
-        steps={projectSteps.map(projectStep => ({
-          name: projectStep.step.moduleType
-        }))}
-        activeStep={activeStep}
-        handleNext={handleNext}
-        handleBack={handleBack}
-      />
+    <>
+      <div className={classes.workflowContainer}>
+        <VerticalStepIndicator
+          steps={projectSteps.map(projectStep => ({
+            name: projectStep.step.moduleType
+          }))}
+          activeStep={activeStep}
+          handleNext={handleNextStepClick}
+          handleBack={handlePreviousStepClick}
+        />
 
-      <div
-        className={
-          classes.stepContainer +
-          (isSideDrawerOpen ? ' right-squeeze' : isRightSideBarVisible ? ' right-pinch' : '')
-        }
-      >
-        <WorkflowStep projectSteps={projectSteps} activeStep={activeStep} direction={direction} />
+        <div className={classes.stepContainer} ref={stepContainerRef}>
+          <section className={classes.mainPane} ref={mainPaneRef}>
+            <WorkflowStep projectSteps={projectSteps} activeStep={activeStep} direction={direction} />
+          </section>
+
+          {isAdditionalPaneVisible && (
+            <>
+              <div className={classes.verticalPaneDivider} onMouseDown={onMouseDown} />
+
+              <AnimatePresence>
+                <motion.section
+                  className={classes.additionalPane}
+                  ref={additionalPaneRef}
+                  initial={{ x: '100vw', opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                  exit={{ x: '100vw', opacity: 0 }}
+                  transition={{
+                    y: { duration: 0.3, type: 'tween' },
+                    opacity: { duration: 0.3 }
+                  }}
+                >
+                  <FeatureTabs />
+                </motion.section>
+              </AnimatePresence>
+            </>
+          )}
+        </div>
       </div>
-
-      <SideDrawer anchor="right">
-        <FeatureTabs />
-      </SideDrawer>
-
-      <ThemeProvider theme={sidebarTheme}>
-        {toolBarRenderCondition && (
-          <RightSideBarContainer>
-            <RightSideBarContent />
-          </RightSideBarContainer>
-        )}
-      </ThemeProvider>
-    </div>
+      {isAdditionalPaneVisible ? (
+        <Fab
+          className={classes.toolAreaToggle}
+          onClick={handleCloseAdditionalPane}
+          size="small"
+          color="primary"
+          aria-label="toggle"
+        >
+          <CloseIcon />
+        </Fab>
+      ) : (
+        <SpeedDial
+          ariaLabel="tools"
+          className={classes.toolAreaToggle}
+          icon={<SpeedDialIcon />}
+          onClose={() => setIsSpeedDialOpen(false)}
+          onOpen={() => setIsSpeedDialOpen(true)}
+          open={isSpeedDialOpen}
+          direction="up"
+          FabProps={{ size: 'small' }}
+        >
+          {featureList.map((feature, index) => (
+            <SpeedDialAction
+              key={feature.id}
+              icon={feature.icon}
+              tooltipTitle={feature.tooltip}
+              onClick={() => {
+                console.log(`${feature.label} clicked`)
+                setFeatureTabIndex(index)
+                handleOpenAdditionalPane()
+              }}
+            />
+          ))}
+        </SpeedDial>
+      )}
+    </>
   )
 }
 
