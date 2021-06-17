43e89b2af9655425a7a4c962ea05d7ae1d752020
dandybytes <42968783+dandybytes@users.noreply.github.com>
2021-06-17 09:11:43 +0200

    Minor MUI theme updates

---
+++ packages/client-ui/src/theme/muiDefaultProps.ts
@@ -0,0 +1,20 @@
+import { ComponentsProps } from '@material-ui/core/styles/props'
+
+export default {
+  MuiCard: {
+    variant: 'outlined'
+  },
+  MuiCardActionArea: {
+    disableRipple: true
+  },
+  MuiCheckbox: {
+    disableRipple: true
+  },
+  MuiPaper: {
+    variant: 'outlined',
+    elevation: 0
+  },
+  MuiSvgIcon: {
+    fontSize: 'small'
+  }
+} as ComponentsProps
