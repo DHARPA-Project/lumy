3ba844eb0d178cf98b11a7d5c740b761e0ae22c2
dandybytes <42968783+dandybytes@users.noreply.github.com>
2021-06-18 17:40:08 +0200

    Make graph statistics panel collapsible

--- packages/modules/src/networkAnalysisDataVis/components/statistics/GraphStatsPanel.styles.ts
+++ packages/modules/src/networkAnalysisDataVis/components/statistics/GraphStatsPanel.styles.ts
@@ -1,12 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles'
 
 export default makeStyles(theme => ({
-  cardRoot: {
-    marginBottom: theme.spacing(1)
-  },
-  cardContentRoot: {
-    padding: 0
-  },
   tableCellRoot: {
     borderBottom: 'none',
     paddingBottom: theme.spacing(0.5)
