f287a0457c6c1624f995da397221304c55549c98
dandybytes <42968783+dandybytes@users.noreply.github.com>
2021-07-05 19:31:56 +0200

    Enable querying feature tab panels by ID

--- packages/client-ui/src/const/features.tsx
+++ packages/client-ui/src/const/features.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import CreateIcon from '@material-ui/icons/Create'
 import CodeIcon from '@material-ui/icons/Code'
 import TableChartIcon from '@material-ui/icons/TableChart'
+import LibraryBooksIcon from '@material-ui/icons/LibraryBooks'
 
 import NoteEditor from '../components/common/NoteEditor'
 import DataPreview from '../components/common/DataPreview'
@@ -16,26 +17,40 @@ export interface IFeature {
   content: JSX.Element
 }
 
+export const featureIds = {
+  note: 'note-feature-container',
+  data: 'data-view-container',
+  code: 'code-view-container',
+  documentation: 'documentation-container'
+}
+
 export const featureList: IFeature[] = [
   {
-    id: 'code',
-    label: 'Code',
-    tooltip: 'View Code',
-    icon: <CodeIcon />,
-    content: <CodeView />
+    id: featureIds.note,
+    label: 'Notes',
+    tooltip: 'Check Notes',
+    icon: <CreateIcon />,
+    content: <NoteEditor />
   },
   {
-    id: 'data',
+    id: featureIds.data,
     label: 'Data',
     tooltip: 'View Data',
     icon: <TableChartIcon />,
     content: <DataPreview />
   },
   {
-    id: 'notes',
-    label: 'Notes',
-    tooltip: 'Check Notes',
-    icon: <CreateIcon />,
-    content: <NoteEditor />
+    id: featureIds.code,
+    label: 'Code',
+    tooltip: 'View Code',
+    icon: <CodeIcon />,
+    content: <CodeView />
+  },
+  {
+    id: featureIds.documentation,
+    label: 'Guide',
+    tooltip: 'See Documentation',
+    icon: <LibraryBooksIcon />,
+    content: null
   }
 ]
