2b16565f8d73416b518db68fa3545803b58cd8d1
dandybytes <42968783+dandybytes@users.noreply.github.com>
2021-07-08 11:59:16 +0200

    Make the data source content preview render in a persisitent, scrollable popover (#121)
    * Content preview in persisitent scrollable popover
    
    * Optimize mock data rendering

--- packages/modules/src/networkAnalysisDataVis/components/settings/popover/SettingMenuPopover.styles.ts
+++ packages/modules/src/networkAnalysisDataVis/components/settings/popover/SettingMenuPopover.styles.ts
@@ -1,9 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles'
 
 export default makeStyles(theme => ({
-  popover: {
-    position: 'relative'
-  },
   list: {
     width: 'auto',
     minWidth: 260,
