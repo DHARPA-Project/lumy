ee5993768811be7c3e7cbdf9a2b6eca9f50736b0
dandybytes <42968783+dandybytes@users.noreply.github.com>
2021-07-15 10:49:45 +0200

    Network analysis graph type selectio in two stages

--- packages/client-ui/src/components/pages/NetworkAnalysisPage.styles.ts
+++ packages/client-ui/src/components/pages/NetworkAnalysisPage.styles.ts
@@ -24,33 +24,9 @@ export default makeStyles(theme => ({
     gridTemplateColumns: 'auto auto',
     gridGap: theme.spacing(3)
   },
-  projects: {
-    minHeight: '5rem',
-    gridArea: 'projects',
-    display: 'flex',
-    flexDirection: 'column'
-  },
-  createProjectButton: {
-    margin: `${theme.spacing(3)}px auto ${theme.spacing(3)}px`
-  },
-  paragraphPlaceholder: {},
   workflowTypes: {
     display: 'flex',
     justifyContent: 'center',
-    alignItems: 'center'
-  },
-  cardWrapper: {
-    maxWidth: theme.spacing(35),
-    margin: theme.spacing(2),
-    textDecoration: 'none'
-  },
-  cardImage: {
-    display: 'grid',
-    placeItems: 'center',
-    aspectRatio: '1/1'
-  },
-  cardActions: {
-    display: 'grid',
-    placeItems: 'center'
+    alignItems: 'stretch'
   }
 }))
