8e1235f9578859c556ee878143715aa70f1421d1
dandybytes <42968783+dandybytes@users.noreply.github.com>
2021-07-29 16:05:09 +0200

    feature/network-analysis-options (#138)
    * Option card style improvements
    
    * Update network type selection steps
    
    * Add svg loader to webpack specs for the Jupyter app
    
    * Remove redundant URLSearchParams polyfill

--- packages/client-ui/src/modules.d.ts
+++ packages/client-ui/src/modules.d.ts
@@ -1,6 +1,14 @@
-// declare modules that do not provide types in order to suppress Typescript warnings
+/**
+ * declare modules here that do not provide types in order to suppress Typescript warnings
+ */
+
 declare module 'react-jupyter-notebook'
 declare module '*.ipynb' {
   const src: string
   export default src
+}
+
+declare module '*.svg' {
+  const content: string
+  export default content
 }
