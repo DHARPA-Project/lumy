f2367961f627b61add5a8ca584d644697cd312b6
Roman Kalyakin <roman@kalyakin.com>
2021-08-03 11:10:20 +0200

    linter fixes

--- packages/modules/src/networkAnalysisDataVis/components/NetworkAnalysisVisualization.styles.ts
+++ packages/modules/src/networkAnalysisDataVis/components/NetworkAnalysisVisualization.styles.ts
@@ -1,6 +1,28 @@
 import { makeStyles, Theme } from '@material-ui/core/styles'
 
-export default makeStyles((theme: Theme) => ({
+/**
+ * TODO: this is already declared in `client-ui`. Consider
+ * moving theme handling to a separate package that can be referenced
+ * here.
+ */
+interface CustomTheme extends Theme {
+  layout: {
+    sideBarFullWidth: React.CSSProperties['width']
+    sideBarCollapsedWidth: React.CSSProperties['width']
+    navLinkTextWidth: React.CSSProperties['width']
+    toolBarWidth: React.CSSProperties['width']
+    navBarTop: React.CSSProperties['height']
+    navBarBottom: React.CSSProperties['height']
+    pageHeaderHeight: React.CSSProperties['height']
+    pagePadding: React.CSSProperties['padding']
+    toolContainerWidth: React.CSSProperties['width']
+    scrollBarWidth: React.CSSProperties['width']
+    tabHeight: React.CSSProperties['width']
+    paneDividerWidth: React.CSSProperties['width']
+  }
+}
+
+export default makeStyles((theme: CustomTheme) => ({
   visualizationContainer: {
     height: '100%',
     display: 'grid',
