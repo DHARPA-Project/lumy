4131df07de88ebe598ee32cccd364b0b5fb80c88
theorm <theorm@gmail.com>
2021-08-10 14:48:16 +1000

    removed duplicate interfaces

--- packages/styles/src/theme/index.ts
+++ packages/styles/src/theme/index.ts
@@ -5,28 +5,7 @@ import '@fontsource/roboto'
 import muiDefaultProps from './muiDefaultProps'
 import getMuiThemeOverrides from './muiThemeOverrides'
 
-interface LayoutThemeProps {
-  sideBarFullWidth: React.CSSProperties['width']
-  sideBarCollapsedWidth: React.CSSProperties['width']
-  navLinkTextWidth: React.CSSProperties['width']
-  toolBarWidth: React.CSSProperties['width']
-  navBarTop: React.CSSProperties['height']
-  navBarBottom: React.CSSProperties['height']
-  pageHeaderHeight: React.CSSProperties['height']
-  pagePadding: React.CSSProperties['padding']
-  toolContainerWidth: React.CSSProperties['width']
-  scrollBarWidth: React.CSSProperties['width']
-  tabHeight: React.CSSProperties['width']
-  paneDividerWidth: React.CSSProperties['width']
-}
-
-export interface LumyTheme extends Theme {
-  layout?: LayoutThemeProps
-}
-
-export interface LumyThemeOptions extends ThemeOptions {
-  layout?: LayoutThemeProps
-}
+export type { LumyTheme, LumyThemeOptions } from './theme'
 
 // the default/base Material UI theme object
 const muiThemeBase = createMuiTheme()
