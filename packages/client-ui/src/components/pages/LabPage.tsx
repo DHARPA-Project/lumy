d7d4259eb75ff80e8adc982d84c7cc0d02bfcbb0
dandybytes <42968783+dandybytes@users.noreply.github.com>
2021-08-24 15:21:40 +0200

    Create utils package

--- packages/client-ui/src/components/pages/LabPage.tsx
+++ packages/client-ui/src/components/pages/LabPage.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
+import { generateUniqueId } from '@lumy/utils '
+
 import WorkflowContextProvider from '../../context/workflowContext'
 
 import WorkflowContainer from '../common/WorkflowContainer'
 
 const LabPage = (): JSX.Element => {
+  console.log(`unique id: `, generateUniqueId?.())
+
   return (
     <WorkflowContextProvider>
       <WorkflowContainer />
