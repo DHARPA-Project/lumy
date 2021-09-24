1f963c97a40132922844f547c5d13c329dececee
dandybytes <42968783+dandybytes@users.noreply.github.com>
2021-09-24 18:07:41 +0200

    Remove redundant toggler

--- applications/jupyter-app/src/webapp/installer/index.tsx
+++ applications/jupyter-app/src/webapp/installer/index.tsx
@@ -6,17 +6,9 @@ import { ElectronContext } from './context'
 const SplashScreenApp = (): JSX.Element => {
   const context = React.useRef(new ElectronContext())
 
-  const handleLogToggle = (isExpaned: boolean) => {
-    if (isExpaned) window.resizeTo(700, 600)
-    else window.resizeTo(700, 300)
-  }
   return (
     <SplashScreenContextProvider value={context.current}>
-      <SplashScreen
-        title="Lumy"
-        subtitle="Hang tight, installing dependencies"
-        onLogToggle={handleLogToggle}
-      />
+      <SplashScreen title="Lumy" subtitle="Data science research toolkit" />
     </SplashScreenContextProvider>
   )
 }
