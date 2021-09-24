584bded0fb33a72a6eb94c84cec6bbf922ddd0ef
dandybytes <42968783+dandybytes@users.noreply.github.com>
2021-09-24 17:25:04 +0200

    Spruce up SplashScreen layout & style

--- packages/splash-screen/src/components/SplashScreen.tsx
+++ packages/splash-screen/src/components/SplashScreen.tsx
@@ -1,36 +1,37 @@
 import React from 'react'
-import {
-  Grid,
-  Typography,
-  CircularProgress,
-  Accordion,
-  AccordionSummary,
-  AccordionDetails,
-  Avatar
-} from '@material-ui/core'
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
-import { LogContainer } from './LogContainer'
+
+import { Grid, Typography, CircularProgress, Avatar } from '@material-ui/core'
+
 import useStyles from './SplashScreen.styles'
 import { useFinishMessage } from '../context'
+
+import { LogContainer } from './LogContainer'
 
 export interface SplashScreenProps {
   title: string
   subtitle?: string
-  onLogToggle?: (isExpanded: boolean) => void
 }
 
-export const SplashScreen = ({ title, subtitle, onLogToggle }: SplashScreenProps): JSX.Element => {
-  const [isLogExpanded, setLogExpanded] = React.useState(false)
+export const SplashScreen = ({ title, subtitle }: SplashScreenProps): JSX.Element => {
   const classes = useStyles()
+
   const finishMessage = useFinishMessage()
 
-  React.useEffect(() => onLogToggle?.(isLogExpanded), [isLogExpanded])
-
   return (
-    <div className={classes.root}>
-      <Grid container direction="row" wrap="nowrap" className={classes.container}>
-        {/* left */}
-        <Grid item className={classes.leftColumn}>
+    <Grid container direction="row" className={classes.container}>
+      {/* left */}
+      <Grid
+        item
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        wrap="nowrap"
+        xs={12}
+        md={6}
+        className={classes.leftColumn}
+      >
+        <Grid item>
           <Avatar
             className={classes.avatar}
             alt="Mechanical Turk"
@@ -38,61 +39,52 @@ export const SplashScreen = ({ title, subtitle, onLogToggle }: SplashScreenProps
             src="https://dharpa.org/img/story/mechTurk_color.jpg"
           />
         </Grid>
-        {/* right */}
-        <Grid item className={classes.rightColumn}>
-          <Grid
-            container
-            direction="column"
-            justify="center"
-            alignContent="center"
-            spacing={2}
-            wrap="nowrap"
-            className={classes.mainSection}
-          >
-            <Grid item>
-              <Typography variant="caption" component="h1" className={classes.header}>
-                {title}
-              </Typography>
-            </Grid>
-            <Grid item>
-              {subtitle != null ? (
-                <Typography variant="caption" component="h2" className={classes.subheader}>
-                  {subtitle}
-                </Typography>
-              ) : (
-                ''
-              )}
-            </Grid>
-            <Grid item className={classes.spinnerContainer}>
-              {finishMessage == null ? (
-                <CircularProgress color="primary" />
-              ) : (
-                <Typography
-                  align="center"
-                  variant="caption"
-                  color={finishMessage?.type === 'ok' ? 'textPrimary' : 'error'}
-                >
-                  {finishMessage?.text}
-                </Typography>
-              )}
-            </Grid>
-            <Grid item className={classes.logContainerWrapper}>
-              <Accordion
-                square
-                className={classes.accordion}
-                onChange={(_, isExpanded) => setLogExpanded(isExpanded)}
-              >
-                <AccordionSummary expandIcon={<ExpandMoreIcon />} className={classes.accordionSummary}>
-                  <Typography variant="button">{isLogExpanded ? 'Hide log' : 'Show log'}</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                  <LogContainer className={classes.logContainer} hideWhenEmpty />
-                </AccordionDetails>
-              </Accordion>
-            </Grid>
-          </Grid>
+
+        <Grid item>
+          <Typography variant="caption" component="h1" className={classes.header}>
+            {title}
+          </Typography>
+        </Grid>
+
+        <Grid item>
+          {subtitle != null && (
+            <Typography variant="caption" component="h2" className={classes.subheader}>
+              {subtitle}
+            </Typography>
+          )}
         </Grid>
       </Grid>
-    </div>
+
+      {/* right */}
+      <Grid
+        item
+        container
+        direction="column"
+        justifyContent="flex-start"
+        alignItems="center"
+        wrap="nowrap"
+        xs={12}
+        md={6}
+        className={classes.rightColumn}
+      >
+        <Grid item className={classes.appLoadStatus}>
+          {finishMessage == null ? (
+            <CircularProgress color="primary" />
+          ) : (
+            <Typography
+              align="center"
+              variant="caption"
+              color={finishMessage?.type === 'ok' ? 'textPrimary' : 'error'}
+            >
+              {finishMessage?.text}
+            </Typography>
+          )}
+        </Grid>
+
+        <Grid item className={classes.logContainerWrapper}>
+          <LogContainer hideWhenEmpty />
+        </Grid>
+      </Grid>
+    </Grid>
   )
 }
