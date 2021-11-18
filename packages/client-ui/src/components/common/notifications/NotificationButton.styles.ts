0d122784e333f128cc80315b227e296bdee95223
dandybytes <42968783+dandybytes@users.noreply.github.com>
2021-11-18 17:16:34 +0100

    Remove redundant class names

--- packages/client-ui/src/components/common/notifications/NotificationButton.styles.ts
+++ packages/client-ui/src/components/common/notifications/NotificationButton.styles.ts
@@ -3,7 +3,7 @@ import { Theme } from '@lumy/styles'
 
 export default makeStyles((theme: Theme) => ({
   notificationButton: {
-    position: 'fixed',
+    position: 'absolute',
     top: theme.spacing(2),
     left: theme.spacing(0.5),
     zIndex: theme.zIndex.speedDial
@@ -16,11 +16,5 @@ export default makeStyles((theme: Theme) => ({
     width: `clamp(${theme.spacing(40)}px, 50vw, ${theme.spacing(100)}px)`,
     border: 'none',
     position: 'absolute !important' as 'absolute'
-  },
-  drawerBackdropRoot: {
-    // position: 'absolute !important' as 'absolute'
-  },
-  drawerRoot: {
-    // position: 'absolute !important' as 'absolute'
   }
 }))
