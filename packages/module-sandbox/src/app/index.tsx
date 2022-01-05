a91a619e5e3e87f7b1fbe5e4318a7a0588e03ab6
dandybytes <42968783+dandybytes@users.noreply.github.com>
2022-01-05 17:51:56 +0100

    Render sandbox app in dedicated container (#198)

--- packages/module-sandbox/src/app/index.tsx
+++ packages/module-sandbox/src/app/index.tsx
@@ -3,5 +3,12 @@ import { render } from 'react-dom'
 import { App } from './App'
 
 export function sandbox(): void {
-  render(<App />, document.body)
+  let sandboxRootElement = document.getElementById('sandbox')
+
+  if (sandboxRootElement == null) {
+    sandboxRootElement = document.createElement('div')
+    sandboxRootElement.setAttribute('id', 'sandbox')
+    document.querySelector('body').appendChild(sandboxRootElement)
+  }
+  render(<App />, sandboxRootElement)
 }
